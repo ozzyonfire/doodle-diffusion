cd62d19cc34f87f0a52fc59bd832bf8458cacb5e
Matt <mattoskamp@gmail.com>
2023-03-01 09:29:08 -0500

    typings

--- pages/api/doodle/index.ts
+++ pages/api/doodle/index.ts
@@ -1,10 +1,11 @@
 import { Doodle, saveDoodle } from "@/model/doodle";
 import { NextApiHandler } from "next";
 
+// only use this API enpoint when creating a new doodle
 const handler: NextApiHandler = async (req, res) => {
   // if this is a POST request, save the doodle to the database
   if (req.method === "POST") {
-    const { input, output, prompt, predictionId, userId, upvotes, downvotes } = req.body;
+    const { input, output, prompt, predictionId, userId } = req.body;
 
     const doodle: Doodle = {
       date: new Date().toISOString(),
@@ -13,8 +14,8 @@ const handler: NextApiHandler = async (req, res) => {
       output,
       predictionId,
       userId,
-      upvotes,
-      downvotes
+      upvotes: [], // todo: make it so we don't overwrite the upvotes on a save
+      downvotes: []
     };
 
     const response = await saveDoodle(doodle);
